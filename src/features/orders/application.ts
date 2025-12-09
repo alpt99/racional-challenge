42fe47e4365326ebfede6339933a43e9a8c5c58b
Alejandro Lopez <alpt@uc.com>
2025-12-09 20:45:10 -0300

    deleted some files

--- src/features/orders/application.ts
+++ src/features/orders/application.ts
@@ -6,7 +6,6 @@ import {
   type OrderSideValue,
 } from "./model";
 import { orderRepository } from "./repository";
-import { portfolioPositionRepository } from "../portfolioPositions/repository";
 import { DomainError } from "@/lib/errors";
 import { portfolioApplication } from "../portfolios/application";
 
@@ -36,33 +35,5 @@ export const orderApplication = {
     console.log("payload", payload);
     const order = await orderRepository.createWithEffects(payload);
     return order;
-  },
-
-  placeOrderV2: async (input: PlaceOrderInput) => {},
-
-  updateOrderStatus: async (input: UpdateOrderStatusInput) => {
-    const payload = updateOrderStatusSchema.parse(input);
-    const updatedOrder = await orderRepository.updateStatus(
-      payload.orderId,
-      payload.status,
-      payload.filledAt
-    );
-
-    if (payload.status === "FILLED") {
-      const side = updatedOrder.side as OrderSideValue;
-      const quantityDelta =
-        side === "BUY"
-          ? Number(updatedOrder.quantity)
-          : -Number(updatedOrder.quantity);
-
-      await portfolioPositionRepository.adjustQuantity({
-        portfolioId: updatedOrder.portfolioId,
-        stockId: updatedOrder.stockId,
-        quantityDelta,
-        price: updatedOrder.price ? Number(updatedOrder.price) : undefined,
-      });
-    }
-
-    return updatedOrder;
   },
 };
